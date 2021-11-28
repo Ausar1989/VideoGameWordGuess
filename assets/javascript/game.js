a1617f1466a7a1fe6947059a9a0ca12443283f7f
Jermaine Lewis <lewisjermaine22@gmail.com>
2021-11-28 15:47:57 -0600

    hmtl css part complete

---
+++ assets/javascript/game.js
@@ -0,0 +1,71 @@
+//This is a giant object in which all of the game's logic and variables will be
+var VideoGameGuessGame = {
+  WordsToPick: {
+    AkuAku: {
+      picture: "AkuAku.jpg",
+      song: "Aku Aku Invincibilty",
+      preview: "https://vgmsite.com/soundtracks/crash-bandicoot-series-sound-box/twuvjgdsxs/01-26.%20Aku%20Aku.mp3"  
+    },
+    Bowser: {
+     picture: "Bowser.jpg",
+     song: "Bowser boss battle",
+     preview: "https://vgmsite.com/soundtracks/super-mario-world-snes-gamerip/jitlswgzmy/07%20Bowser%27s%20Castle%20Emerges.mp3"  
+    },
+    CJ: { 
+    picture: "CJ.jpg",
+    song: "san andreas music",
+    preview:"https://nf1f8200-a.akamaihd.net/downloads/ringtones/files/mp3/gta-san-andreas-6042.mp3"
+    },
+    CrashBandicoot: { 
+    picture:  "CrashBandicoot.jpg",
+    song: "Crash Bandicoot music",
+    preview: "https://vgmsite.com/soundtracks/crash-bandicoot-4-the-wrath-of-cortex-psx-gamerip/mttonthfae/01%20The%20Wrath%20Of%20Cortex.mp3"
+    },
+    DonkeyKong: {
+    picture: "DonkeyKong.jpg",
+    song: "Donkey Kong Song",
+    preview: "https://vgmsite.com/soundtracks/donkey-kong-64-game-music/nixzbfap/Tag%20Barrel%20Donkey.mp3"
+    },
+    DrNeoCortex: {
+    picture: "DrNeoCortex.jpg",
+    song: "villain battle song",
+    preview: "https://vgmsite.com/soundtracks/crash-bandicoot-4-the-wrath-of-cortex-psx-gamerip/nifuamviap/30%20Cortex%20Vortex.mp3"
+    },
+    Franklin: {
+    picture: "Franklin.jpg",
+    song: "gta5 theme song",
+    preview: "https://nf1f8200-a.akamaihd.net/downloads/ringtones/files/mp3/gta-san-andreas-6042.mp3"
+    },
+    Knuckles: {
+    picture: "Knuckles.jpg",
+    song: "knuckles song",
+    preview: "https://s3-us-west-2.amazonaws.com/shq-media/media/games/sonicthehedgehog-8bit/mp3/10%20-%20Jungle%20Zone.mp3"
+    },
+    robotnik: {
+    picture: "robotnik.jpg",
+    song: "Dr Robotnik battle",
+    preview: "https://s3-us-west-2.amazonaws.com/shq-media/media/games/sonicthehedgehog-8bit/mp3/08%20-%20Boss%20Theme.mp3"
+    },
+    SolidSnake: {
+    picture: "SolidSnake.jpg",
+    song: "Metal Gear Solid song",
+    preview: "https://vgmsite.com/soundtracks/metal-gear-solid-virtual-existence/bvvfmzaymi/05%20-%20Objective%20Complete.mp3"
+    },
+    Sonic: {
+    picture: "Sonic.jpg",
+    song: "Sonic The Hedgehog",
+    preview: "https://s3-us-west-2.amazonaws.com/shq-media/media/games/sonicspinball/mp3/Spinball%20Theme.mp3"
+    },
+    SuperMario: {
+    picture: "SuperMario.jpg",
+    song: "Super Mario Song",
+    preview: "https://vgmsite.com/soundtracks/super-mario-world-original-soundtrack/vmrblynm/17%20-%20Invincible%20Bgm.mp3"
+    },
+    Trevor: {
+    picture: "Trevor.jpg",
+    song: "Trevor",
+    preview: "https://nf1f8200-a.akamaihd.net/downloads/ringtones/files/mp3/gta-san-andreas-6042.mp3"
+    }
+  }
+};
+   
